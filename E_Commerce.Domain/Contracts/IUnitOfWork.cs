4da1f2303aed369731446aca55d649fce04e13a1
Abdelrahmankishk <abdelrahmankishk@gmail.com>
2026-06-23 18:34:52 +0300

    add Unit of Work and its Implementation

---
+++ E_Commerce.Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using E_Commerce.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Domain.Contracts
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<T, TKey> GetRepository<T, TKey>()
+        where T : BaseEntity<TKey>;
+
+        Task<int> SaveChangesAsync(CancellationToken ct = default);
+    }
+}
---
+++ E_Commerce.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using E_Commerce.Domain.Common;
+using E_Commerce.Domain.Contracts;
+using E_Commerce.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Infrastructure.Repositories
+{
+    internal class UnitOfWork(StoreDbContext dbContext) : IUnitOfWork
+    {
+        private readonly Dictionary<string, object> repositories = [];
+        public IGenericRepository<T, TKey> GetRepository<T, TKey>() where T : BaseEntity<TKey>
+        {
+            var typeName = typeof(T).Name;
+            if (repositories.TryGetValue(typeName, out object? value))
+                return (IGenericRepository<T, TKey>)value;
+
+            var Repo = new GenericRepository<T, TKey>(dbContext);
+            repositories[typeName] = Repo;
+            return Repo;
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken ct = default)
+        {
+            return await dbContext.SaveChangesAsync(ct);
+        }
+    }
+}
