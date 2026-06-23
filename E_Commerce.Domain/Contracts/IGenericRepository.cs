d97573d60fc618d17f6150d0516271ccc023253c
Abdelrahmankishk <abdelrahmankishk@gmail.com>
2026-06-23 18:17:11 +0300

    Add IGenericRepository and its Implementation

---
+++ E_Commerce.Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using E_Commerce.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Domain.Contracts
+{
+    public interface IGenericRepository<T, TKey> where T : BaseEntity<TKey>
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<T?> GetByIdAsync(TKey id, CancellationToken ct = default);
+        Task<IEnumerable<T>> GetAllAsync(CancellationToken ct = default);
+    }
+}
---
+++ E_Commerce.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using E_Commerce.Domain.Common;
+using E_Commerce.Domain.Contracts;
+using E_Commerce.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Infrastructure.Repositories
+{
+    internal class GenericRepository<T, TKey>(StoreDbContext dbContext) : IGenericRepository<T, TKey> where T : BaseEntity<TKey>
+    {
+        public void Add(T entity)
+        {
+            dbContext.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            dbContext.Set<T>().Remove(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(CancellationToken ct = default)
+        {
+           return await dbContext.Set<T>().ToListAsync(ct);
+        }
+
+        public async Task<T?> GetByIdAsync(TKey id, CancellationToken ct = default)
+        {
+            return await dbContext.Set<T>().FindAsync(id, ct);
+        }
+
+        public void Update(T entity)
+        {
+            dbContext.Set<T>().Update(entity);
+        }
+    }
+}
