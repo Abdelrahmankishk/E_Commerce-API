2b4491917aef642e168fbbbf865494d0e2062ff6
Abdelrahmankishk <abdelrahmankishk@gmail.com>
2026-06-27 12:43:31 +0300

    add Basket Service Contract & Basket Dtos

---
+++ E_Commerce.Application/DTOs/Basketd/BasketItemDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.Application.DTOs.Basketd
+{
+    public class BasketItemDto
+    {
+        [Required(ErrorMessage = "Product ID is Reduired")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Product Name is Reduired")]
+        public string ProductName { get; set; } = default!;
+        public string ProductUrl { get; set; } = default!;
+        [Range(1,double.MaxValue)]
+        public decimal Price { get; set; }
+        [Range(1,50)]
+        public int Quantity { get; set; }
+    }
+}
